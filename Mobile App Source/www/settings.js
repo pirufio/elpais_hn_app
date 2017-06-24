8f26747506ceca1a254adb829dcbf6c701ee0b5f
pirufio <francoandresvalle@gmail.com>
2017-06-24 12:21:51 -0300

    commit inicial

---
+++ Mobile App Source/www/settings.js
@@ -0,0 +1,18 @@
+//For rate and share app
+var android_packageName = "com.taydo.dailynews";
+var apple_id = "1166773798";
+//URL site wordpress
+var wordpress_url = "http://taydomailer.com/mobileapp/dailynews";
+//Posts per page
+var wordpress_per_page = 10;
+//For push notification with OneSignal
+var onesignal_app_id = "b315f4b1-83ea-4d2a-bc34-ca3ba56cb23a";
+var onesignal_project_number = "190957962003";
+//For RTL Language
+var rtl_language = false;
+//Open link in Detail
+var open_target_blank = true;
+//Date time format
+var date_format = "dd-MM-yyyy";
+//For Google Analytics
+var google_analytics = "UA-85795435-1";
